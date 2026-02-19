d8ef9764ce42663640c5e0a2025c8732ebd4b071
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-18 22:41:26 -0300

    fix: order-queue in orders_queue

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -11,7 +11,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.RMQ,
         options: {
           urls: ['amqp://admin:admin@localhost:5672'],
-          queue: 'orders-queue',
+          queue: 'orders_queue',
           queueOptions: {
             durable: true,
           },
