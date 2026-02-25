e1da2b76148c78814a8ad79ae87eaa1251216589
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-24 22:14:51 -0300

    refactor(order-service): remove ClientProxy and use exchange-based publish

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,16 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/CreateOrderDto';
+import { RabbitMQProvider } from 'src/providers/RabbitMQProvider';
 
 @Injectable()
 export class OrderService {
-  constructor(
-    @Inject('RABBITMQ_SERVICE')
-    private readonly client: ClientProxy,
-  ) {}
+  constructor(private readonly rabbitMQProvider: RabbitMQProvider) {}
 
   save(order: CreateOrderDto) {
-    this.client.emit('order_created', order);
+    this.rabbitMQProvider.publish('orders.exchange', 'order.created', order);
     return order;
   }
 }
