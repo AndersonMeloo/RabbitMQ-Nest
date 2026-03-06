6914938e7b693b9b78e0157c6c44af6863bd0c77
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-05 22:12:07 -0300

    refactor: Connecting rabbitURL and routingKey with.env

--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppController);
+  const configService = appContext.get(ConfigService);
+  const routingKey = configService.getOrThrow<string>('RABBIT_ORDER_KEY');
+
+  const rabbitUrl = configService.getOrThrow<string>('RABBITMQ_URL');
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://admin:admin@localhost:5672'],
+      urls: [rabbitUrl],
       queue: 'payment.order.created',
       exchange: 'orders.exchange',
       exchangeType: 'topic',
-      routingKey: 'order.created',
+      routingKey,
       queueOptions: {
         durable: true,
       },
