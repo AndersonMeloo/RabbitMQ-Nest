4dd79da041b54c493785136029ce1ad078845614
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-05 21:51:46 -0300

    update(queue) payament.order.created

--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: ['amqp://admin:admin@localhost:5672'],
-      queue: 'orders_queue',
+      queue: 'payment.order.created',
       exchange: 'orders.exchange',
       exchangeType: 'topic',
       routingKey: 'order.created',
