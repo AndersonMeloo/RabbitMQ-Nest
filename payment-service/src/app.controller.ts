c2cd78c4540607a6bf3b4c0dde1cc15ab4e60cf1
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-06 23:16:48 -0300

    refactor(payment): move payment processing from controller to service and add retry logic

--- payment-service/src/app.controller.ts
+++ payment-service/src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { KiwifyGateway } from './gateways/payment/KiwifyGateway';
 import { CreatedOrderDto } from './dtos/CreatedOrderDto';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly kwifyGateway: KiwifyGateway) {}
+  constructor(private readonly appService: AppService) {}
   @EventPattern('order.created')
-  payment(order: CreatedOrderDto) {
-    this.kwifyGateway.paymentProccess(order);
+  async payment(order: CreatedOrderDto) {
+    await this.appService.process(order);
   }
 }
