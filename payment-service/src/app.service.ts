4f4d8637d33315807a6f549dad9104454cf5f6b6
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-06 23:10:54 -0300

    feat: add retry mechanism for payment processing

--- payment-service/src/app.service.ts
+++ payment-service/src/app.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { CreatedOrderDto } from '../../payment-service/src/dtos/CreatedOrderDto';
+import { KiwifyGateway } from './gateways/payment/KiwifyGateway';
+
+const MAX_ATTEMPTS = 3;
+const DELAY = 5000;
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(private readonly kiwifyGateway: KiwifyGateway) {}
+
+  async process(order: CreatedOrderDto) {
+    let lastError: Error = new Error('Unknown error');
+
+    for (let attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
+      try {
+        return this.kiwifyGateway.paymentProccess(order);
+      } catch (err) {
+        lastError = err as Error;
+        console.log(`Attempt ${attempt} Retrying in ${DELAY / 1000} seconds`);
+        await new Promise((resolve) => setTimeout(resolve, DELAY));
+      }
+    }
+    throw lastError;
+  }
+}
+
+// Tentativa 1 → erro → espera 5s
+// Tentativa 2 → erro → espera 5s
+// Tentativa 3 → erro → lança erro final
