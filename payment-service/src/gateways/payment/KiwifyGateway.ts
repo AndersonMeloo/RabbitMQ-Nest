2cea945260a66db3acf8cc3dc29e63fe80abf352
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-06 23:23:17 -0300

    test(payment): simulate gateway failure to validate retry logic

--- payment-service/src/gateways/payment/KiwifyGateway.ts
+++ payment-service/src/gateways/payment/KiwifyGateway.ts
@@ -4,6 +4,7 @@ import { CreatedOrderDto } from '../../dtos/CreatedOrderDto';
 @Injectable()
 export class KiwifyGateway {
   paymentProccess(data: CreatedOrderDto) {
+    throw new Error('Error on payment gateway');
     console.log('Processing payment with KiwifyGateway', data);
   }
 }
