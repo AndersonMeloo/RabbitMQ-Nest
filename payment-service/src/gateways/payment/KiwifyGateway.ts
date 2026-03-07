8fe0264a01d55d4c2a842b3d8f35fc6117ee455f
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-07 19:25:56 -0300

    feat: feat: add 4s delay to simulate payment gateway response

--- payment-service/src/gateways/payment/KiwifyGateway.ts
+++ payment-service/src/gateways/payment/KiwifyGateway.ts
@@ -3,7 +3,9 @@ import { CreatedOrderDto } from '../../dtos/CreatedOrderDto';
 
 @Injectable()
 export class KiwifyGateway {
-  paymentProccess(data: CreatedOrderDto) {
+  async paymentProccess(data: CreatedOrderDto) {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
     throw new Error('Error on payment gateway');
     console.log('Processing payment with KiwifyGateway', data);
   }
