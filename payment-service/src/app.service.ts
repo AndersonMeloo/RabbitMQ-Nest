afe41c37aaf32f4eb3f9b1ba7c5b5a8f9543fa96
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-07 19:23:30 -0300

    feat: removendo tratativas de erros MAX_ATTEMPTS e DELAY foi passado para /decorator e importando @Retry()

--- payment-service/src/app.service.ts
+++ payment-service/src/app.service.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreatedOrderDto } from '../../payment-service/src/dtos/CreatedOrderDto';
 import { KiwifyGateway } from './gateways/payment/KiwifyGateway';
-
-const MAX_ATTEMPTS = 3;
-const DELAY = 5000;
+import { Retry } from './decorators/retry.decorator';
 
 @Injectable()
 export class AppService {
   constructor(private readonly kiwifyGateway: KiwifyGateway) {}
 
+  @Retry()
   async process(order: CreatedOrderDto) {
-    let lastError: Error = new Error('Unknown error');
-
-    for (let attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
-      try {
-        return this.kiwifyGateway.paymentProccess(order);
-      } catch (err) {
-        lastError = err as Error;
-        console.log(`Attempt ${attempt} Retrying in ${DELAY / 1000} seconds`);
-        await new Promise((resolve) => setTimeout(resolve, DELAY));
-      }
-    }
-    throw lastError;
+    return await this.kiwifyGateway.paymentProccess(order);
   }
 }
 
