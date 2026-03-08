34f557e4f3e993ec42c8c36dcf723daba58bfabe
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-08 20:57:43 -0300

    feat: new context getChannelRef getMessage amqplib

--- payment-service/src/app.controller.ts
+++ payment-service/src/app.controller.ts
@@ -1,13 +1,24 @@
 import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { Ctx, EventPattern, RmqContext } from '@nestjs/microservices';
 import { CreatedOrderDto } from './dtos/CreatedOrderDto';
 import { AppService } from './app.service';
+import { Channel, Message } from 'amqplib';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @EventPattern('order.created')
-  async payment(order: CreatedOrderDto) {
-    await this.appService.process(order);
+  async payment(order: CreatedOrderDto, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef() as Channel;
+    const originalMsg = context.getMessage() as Message;
+
+    try {
+      await this.appService.process(order);
+      channel.ack(originalMsg);
+    } catch (error) {
+      console.log(error);
+
+      channel.nack(originalMsg, false, false);
+    }
   }
 }
